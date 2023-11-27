b06ebb07d2529f8aefdaf2c7a634bc1b41b7a403
Renata Vieira <rrenattavs@gmail.com>
2023-11-26 21:40:11 -0700

    Method with Strings

---
+++ codeAcademy/Unit02/String.py
@@ -0,0 +1,30 @@
+# Strings and concatenation
+word = "First word"
+word2 = "Second word"
+print(word + " " + word2)
+
+# Escaping characters
+name = "Renata"
+print('I\'m ' + name)
+
+# String methods
+name = "Renata"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+# String formatting
+name = "Renata"
+print(f"I'm {name}")
+
+# Accessing characters
+name = "Renata"
+print(name[0])
+print(name[1])
+print(name[2])
+
+# String slicing
+name = "Renata"
+print(name[0:3])
+print(name[0:4])
+print(name[0:5])
