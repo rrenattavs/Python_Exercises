b6e62419c522e0be2461657a6baa204c6824fbe7
Renata Vieira <rrenattavs@gmail.com>
2023-12-18 22:52:13 -0700

    data methods

--- codeAcademy/Unit02/Date and Time/datetime_.py
+++ codeAcademy/Unit02/Date and Time/datetime_.py
@@ -8,3 +8,15 @@ print(now.day)
 print(now.hour)
 print(now.minute)
 print(now.second)   
+
+# Context from Function Python_Exercises/codeAcademy/Unit02/Date and Time/datetime_.py:datetime
+# codeAcademy/Unit02/Date and Time/datetime_.py:datetime
+def datetime():
+    now = datetime.now()
+    print(now)
+    print(now.year)
+    print(now.month)
+    print(now.day)
+    print(now.hour)
+    print(now.minute)
+    print(now.second)
